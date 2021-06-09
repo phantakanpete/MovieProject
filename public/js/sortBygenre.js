5dbf06df04b5e48d3a09049f4641ecdd7bc011f9
Phantakan Muangbua <phantakanmpete@gmail.com>
2021-06-09 15:03:24 +0700

    sortfinish

--- public/js/sortBygenre.js
+++ public/js/sortBygenre.js
@@ -10,4 +10,22 @@ let actionb = document.querySelector('#actionb'),
     adventureb = document.querySelector('#adventureb'),
     comedyb = document.querySelector('#comedyb');
 
-actionb.add
+actionb.addEventListener('click', function(){
+    action.scrollIntoView();
+});
+
+thrillerb.addEventListener('click', function(){
+    thriller.scrollIntoView();
+});
+
+scifib.addEventListener('click', function(){
+    scifi.scrollIntoView();
+});
+
+adventureb.addEventListener('click', function(){
+    adventure.scrollIntoView();
+});
+
+comedyb.addEventListener('click', function(){
+    comedy.scrollIntoView();
+});
